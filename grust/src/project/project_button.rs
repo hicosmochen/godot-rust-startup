d3758bfff4c3e552030a7530f7a6728eb7129563
chenhaochuan <haochuan.chen@itronsvs.com>
2026-03-25 10:54:21 +0800

    feat: 新增rust的枚举给godot外部选择

--- grust/src/project/project_button.rs
+++ grust/src/project/project_button.rs
@@ -1,11 +1,28 @@
 use godot::prelude::*;
 use godot::classes::{Button, IButton, Theme}; // 导入需要的 UI 类
 
+
+// 定义 Button 的枚举类型, 让godot可以进行外部赋值（赋值的内容是指定的数据值）
+#[derive(GodotConvert, Var, Export, Default, Copy, Clone, Debug)]
+// 告诉 Godot 以整数的形式存储索引
+#[godot(via = i64)]
+pub enum ButtonKind {
+    #[default]
+    SelectWorkSpace,
+    RustRootPath,
+    GdextFileName,
+    NeedCreateDemo,
+    StartCreatProject,
+}
+
+
 #[derive(GodotClass)]
 #[class(base=Button)] 
 pub struct ProjectButton {
     pub base: Base<Button>,
-    pub kind: String
+
+    #[export]
+    pub kind: ButtonKind,
 }
 
 
@@ -15,7 +32,7 @@ impl IButton for ProjectButton {
         godot_print!("按钮"); 
         Self {
             base,
-            kind: "default".to_string(),   // 默认初始化
+            kind: ButtonKind::SelectWorkSpace,   // 默认初始化
         }
     }
 
@@ -30,7 +47,16 @@ impl IButton for ProjectButton {
 #[godot_api]
 impl ProjectButton {
     #[func]
-    fn on_button_pressed(){
-         godot_print!("按钮被点击了..."); 
+    fn on_button_pressed(&self){
+         // 根据 kind 执行逻辑
+        match self.kind {
+            ButtonKind::SelectWorkSpace => godot_print!("按钮被点击了...SelectWorkSpace"),
+            ButtonKind::RustRootPath => godot_print!("按钮被点击了...RustRootPath"),
+            ButtonKind::GdextFileName => godot_print!("按钮被点击了...GdextFileName"),
+            ButtonKind::NeedCreateDemo => godot_print!("按钮被点击了...NeedCreateDemo"),
+            ButtonKind::StartCreatProject => godot_print!("按钮被点击了...StartCreatProject"),
+        } 
     }
 }
+
+
